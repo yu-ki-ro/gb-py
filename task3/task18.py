0a44dda7114a727c0f1c12432937a33249cc8571
yukiro <yudanovk@yandex.ru>
2023-03-23 10:39:01 +0300

    18

---
+++ task3/task18.py
@@ -0,0 +1,16 @@
+from random import randint
+
+n = int(input("введите n: "))
+num_list = []
+for _ in range(n):
+    num_list.append(_+1)
+
+x = int(input("введите x: "))
+x_count = 0
+for _ in range(n):
+    if num_list[_] == x-1 :
+        x_count = _+1
+
+print(x_count)
+
+print(num_list[:])
