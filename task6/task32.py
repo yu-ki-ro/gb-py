581aa6c7e45246c4edccd456941be42accc65675
yukiro <yudanovk@yandex.ru>
2023-04-14 09:33:39 +0300

    22

--- task6/task32.py
+++ task6/task32.py
@@ -1,17 +1,13 @@
 def find_indexes(arr, min_val, max_val):
-    """
-    Функция возвращает список индексов элементов массива (списка),
-    значения которых принадлежат заданному диапазону (между min_val и max_val)
-    """
     indexes = []
     for i in range(len(arr)):
         if min_val <= arr[i] <= max_val:
             indexes.append(i)
     return indexes
 
-# Пример использования:
-arr = [1, 5, 10, 15, 20, 25]
-min_val = 10
+
+arr = [3, 8, 55, 34, 20, 86]
+min_val = 4
 max_val = 20
 indexes = find_indexes(arr, min_val, max_val)
-print(indexes) # [2, 3, 4]
+print(indexes)
